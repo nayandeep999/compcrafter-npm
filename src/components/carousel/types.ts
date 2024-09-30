ce3767e3b304478f8186c005d60f5443b3e48375
Nayandeep <nayandeepsen@gmail.com>
2024-09-30 13:45:31 +0530

    added carousel component

---
+++ src/components/carousel/types.ts
@@ -0,0 +1,6 @@
+export interface CarouselProps {
+  children: React.ReactNode;
+  size: string;
+  autoSlide?: boolean; // autoSlide is optional
+  autoSlideInterval?: number; // autoSlideInterval is optional
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import "./styles/main.css";
 export * from "./components/accordian"; // Export everything from the accordion folder
-export * from "./components/todo-list" // Export everything from the todo-list folder
+export * from "./components/todo-list"; // Export everything from the todo-list folder
+export * from "./components/carousel"; // Export everything from the carousel folder
